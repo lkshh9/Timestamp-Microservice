4be7c4ad035a2addd6cdc335c39ec0e947494563
lokesh <lokeshmarwal007@gmail.com>
2024-07-23 21:41:53 +0530

    added date parsing API endpoints

--- index.js
+++ index.js
@@ -18,11 +18,39 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+
+  // If the initial date parsing results in an invalid date, the code tries to parse the date 
+  // parameter as a Unix timestamp by converting it to a number (+req.params.date).
+
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date)
+  }
+
+  // If the date is still invalid after the second attempt, the endpoint responds with a JSON 
+  // object containing an error message.
+  
+  if(isInvalidDate(date)){
+    res.json({error : "Invalid Date"})
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc : date.toUTCString()
+  });
 });
+
+app.get('/api', (req,res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc : new Date().toUTCString()
+  });
+})
 
 
 
